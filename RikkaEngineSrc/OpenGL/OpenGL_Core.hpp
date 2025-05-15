4e03849bde9250211a22440c73a6d4b16a886804
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2025-05-15 19:13:41 +0200

    setting up Repo

---
+++ RikkaEngineSrc/OpenGL/OpenGL_Core.hpp
@@ -0,0 +1,10 @@
+#include <string>
+#include "../Types.hpp"
+
+namespace rkk { 
+    class Engine {
+        public:
+        Engine(const std::string& Title, ruint Width, ruint Height) {}
+        ~Engine() {}
+    };
+}
