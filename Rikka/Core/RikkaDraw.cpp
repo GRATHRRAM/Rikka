29e27ad671c57d70ed1c29fbbb0adfcd77fb9007
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2025-08-31 14:09:12 +0200

    Fixed Textures

--- Rikka/Core/RikkaDraw.cpp
+++ Rikka/Core/RikkaDraw.cpp
@@ -144,18 +144,18 @@ void RikkaDraw::Triangle(Shapes::BlendTriangle& Triangle) {
 }
 
 
-void RikkaDraw::Texture(RikkaTexture& Texture) {
+void RikkaDraw::Texture(RikkaTexture& texture) {
     float vertices[] = {
-        // positions          // colors           // texture coords
+        // positions          // colors           // tex coords
          0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
          0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
         -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
         -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
     };
-    
+
     GLuint indices[] = {
         0, 1, 2,
-        0, 2, 3
+        2, 3, 0
     };
 
     GLuint VAO, VBO, EBO;
@@ -164,8 +164,12 @@ void RikkaDraw::Texture(RikkaTexture& Texture) {
     glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -174,18 +178,23 @@ void RikkaDraw::Texture(RikkaTexture& Texture) {
     glEnableVertexAttribArray(1);
 
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2); 
-    
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(2);
 
-    glUseProgram(Shaders.TextureShader.GetShader());
+    GLuint shader = Shaders.TextureShader.GetShader();
+    glUseProgram(shader);
 
-    glBindTexture(GL_TEXTURE_2D, Texture.GetTexture());
-    glBindVertexArray(VAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture.GetTexture());
+
+    GLint samplerLoc = glGetUniformLocation(shader, "uTexture");
+    glUniform1i(samplerLoc, 0);
+
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glBindVertexArray(0);
+    glUseProgram(0);
     
-    glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
+    glDeleteVertexArrays(1, &VAO);
 }
--- Rikka/Core/RikkaTexture.cpp
+++ Rikka/Core/RikkaTexture.cpp
@@ -6,6 +6,8 @@
 #include <stb_image.h>
 
 bool RikkaTexture::LoadTexture(const char* Path, unsigned int WrapMode, unsigned int FilteringMode) {
+    stbi_set_flip_vertically_on_load(true);
+
     int width, height, nrChannels;
     unsigned char *data = stbi_load(Path, &width, &height, &nrChannels, 0);
     if(!data) {
@@ -13,12 +15,25 @@ bool RikkaTexture::LoadTexture(const char* Path, unsigned int WrapMode, unsigned
         return RKK_FALIURE;
     }
 
+    GLenum format;
+    if (nrChannels == 1)
+        format = GL_RED;
+    else if (nrChannels == 3)
+        format = GL_RGB;
+    else if (nrChannels == 4)
+        format = GL_RGBA;
+    else {
+        std::cout << "RikkaTexture -> Unsupported number of channels: " << nrChannels << "\n";
+        stbi_image_free(data);
+        return RKK_FALIURE;
+    }
+
     glGenTextures(1, &Texture);  
     glBindTexture(GL_TEXTURE_2D, Texture);  
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
-    
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, WrapMode);	
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, WrapMode);
     
